2b9103ac05e55f2e9eafab9d7b37d6c608d1a3cf
Karthick1808 <karthickspikey@gmail.com>
2023-05-18 10:09:20 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo apt-get install curl -y
+sudo rm -rvf /usr/local/bin/docker-compose
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.18.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
